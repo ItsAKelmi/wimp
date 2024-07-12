6f65e8926bc013cd87dbaf5dec8fd148d34a6edf
Kelmi <kelmi.koira@pm.me>
2024-07-12 07:44:50 +0300

    Fix disabling hideNav

--- src/wolfery-improved/ui-tweaks/ui-tweaks.ts
+++ src/wolfery-improved/ui-tweaks/ui-tweaks.ts
@@ -1,6 +1,6 @@
 export function hideNav() {
   const charLog = unsafeWindow?.app?.getModule('charLog');
-  const origNav = charLog.getOverlays()['nav'];
+  const origNav = charLog.getOverlays().get('nav');
   charLog.removeOverlay('nav');
   return () => charLog.addOverlay(origNav);
 }
